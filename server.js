e107b046cad306b35179e09fabc99739746a2b1f
Brad Margerrison <bdm770@gmail.com>
2018-10-02 19:45:33 +0100

    Added view with basic input form

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Weather stuff here soon!'))
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
