4f3fa75eead612e887d45fdb1bb590df1eb45033
Brad Margerrison <bdm770@gmail.com>
2018-10-02 20:31:58 +0100

    Added CSS style sheet

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
-
 app.get('/', function (req, res) {
   res.render('index');
 })
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
